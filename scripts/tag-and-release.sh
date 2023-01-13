86b812bd0f029c45a60e0cdaa54e6ff5c8e6581d
Gong Chen <gong@sprig.com>
2023-01-12 22:14:33 -0800

    oops

--- scripts/tag-and-release.sh
+++ scripts/tag-and-release.sh
@@ -34,7 +34,7 @@ release() {
         -H "Accept: application/vnd.github.v3+json" \
         -H "Authorization: token $GH_TOKEN" \
         -d "$(generate_release_payload)" \
-        https://api.github.com/repos/UserLeap/userleap-ios-sdk-releases-test/releases
+        https://api.github.com/repos/gongstr/userleap-ios-sdk-releases-test/releases
 }
 
 main() {
